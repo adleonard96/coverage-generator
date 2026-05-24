3b8c30cd33a3a7ac3969feedffcc6e263955f01d
adleonard96 <alexanderleonard27@gmail.com>
2026-05-24 14:05:11 -0400

    Initialize WPF application structure
    Added `App.xaml` and `App.xaml.cs` to define the application entry point and resources. Updated `AssemblyInfo.cs` with WPF-specific attributes. Created `coverage_generator.csproj` as a .NET WPF project targeting `.NET 10.0-windows`. Added `MainWindow.xaml` and `MainWindow.xaml.cs` to define the main window layout and logic. Included `coverage_generator.slnx` to manage the solution.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace coverage_generator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
